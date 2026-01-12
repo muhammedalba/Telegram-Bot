30e71bc47971ce4d9790c644ac439d8757a5b599
muhammedalbahle <muhammedalbahle@gmail.com>
2026-01-12 16:00:51 +0100

    add clear All Products fan

--- google.js
+++ google.js
@@ -4,18 +4,17 @@ import {
   CREDS_FILE,
   SPREADSHEET_ID,
   GOOGLE_CREDENTIALS_JSON,
-  ENVIRONMENT
+  ENVIRONMENT,
 } from "./config.js";
 
 let creds;
 
-
 try {
-  if (ENVIRONMENT==="development")  {
+  if (ENVIRONMENT === "development") {
     creds = JSON.parse(fs.readFileSync(CREDS_FILE, "utf8"));
   }
   creds = JSON.parse(GOOGLE_CREDENTIALS_JSON);
-  // 
+  //
 } catch (error) {
   console.error("❌ Error loading credentials:", error.message);
   process.exit(1);
@@ -35,10 +34,33 @@ export async function markDealAsPosted(row) {
   row.posted = "yes";
   await row.save();
 }
-//  clear all products from sheet
-export async function clearAllRowsFromSheet() {
+
+export async function clearAllRowsFromSheet(sheetId, keepHeader = true) {
   await doc.useServiceAccountAuth(creds);
-  // const sheet = doc.sheetsByIndex[0];
-  const sheet = doc.sheetsByTitle['angebote'];
-  await sheet.clearAllRows(sheet.sheetId);
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+//  const sheet = doc.sheetsByTitle["angebote"];
+  if (!sheet) {
+    throw new Error("Sheet not found");
+  }
+
+  const totalRows = sheet.rowCount;
+
+  // إذا لم يكن هناك بيانات
+  if (totalRows <= (keepHeader ? 1 : 0)) {
+    return;
+  }
+
+  const startIndex = keepHeader ? 1 : 0;
+
+  await this._makeSingleUpdateRequest("deleteDimension", {
+    range: {
+      sheetId: sheetId,
+      dimension: "ROWS",
+      startIndex: startIndex,
+      endIndex: totalRows,
+    },
+  });
 }
+
+
